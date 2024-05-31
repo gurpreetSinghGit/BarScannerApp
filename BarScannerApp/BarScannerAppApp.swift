98727b48c872383f93246264a4248ccf9c44cf9e
gurpreetSinghGit <gurpreet.singh@elysiumlabs.in>
2024-05-31 16:24:48 +0530

    Initial Commit

---
+++ BarScannerApp/BarScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarScannerAppApp.swift
+//  BarScannerApp
+//
+//  Created by NewsBytes on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BarScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
